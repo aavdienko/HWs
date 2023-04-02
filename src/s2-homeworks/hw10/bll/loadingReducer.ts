0c3a135debeda78831ee7893e14ed7eac598cb35
aavdienko <asiavostok@seznam.cz>
2023-04-02 17:04:01 +0200

    HW 10. Redux. All done. Ready for deploy.

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,20 @@
 const initState = {
-    isLoading: false,
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type InitialStateType = {
+    isLoading: boolean
+}
+
+export const loadingReducer = (state = initState, action: LoadingActionType): InitialStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+           return {
+            ...state,
+            isLoading: action.isLoading
+           }
+        }
 
         default:
             return state
