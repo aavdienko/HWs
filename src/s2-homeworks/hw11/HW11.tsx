45f5aca4fb496a5e10badf0b9261e13a9248c7b4
aavdienko <asiavostok@seznam.cz>
2023-04-07 17:30:36 +0200

    HW11. bug fixing. deploy.

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import { restoreState, saveState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -12,22 +12,34 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 10))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [0, 100]))
 
     const change = (event: Event, value: number | Array<number>)  => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-        if (Array.isArray(value)) {
-            setValue1(value[0])
-            setValue2(value[1])
-        } else {
+        const minDistance = 5
+        if (!Array.isArray(value)) {
+            saveState('hw11-value1', value)
             setValue1(value)
+            saveState('hw11-value2', [value, value2[1]])
+            setValue2([value, value2[1]])
+
+        } else {
+            saveState('hw11-value1', value[0])
+            setValue1(value[0])
+
+            setValue2(value)
         }
     }
 
     return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+        <div id={'hw11'} className={s.main}>
+
+            <div className={s.titleBlock}>
+                <div className={s2.hwTitle}>Homework #11</div>
+                <hr className={s.hrBottom}/>
+            </div>
+
 
             <div className={s2.hw}>
                 <div className={s.container}>
@@ -40,20 +52,22 @@ function HW11() {
                             onChange={change}
                         />
                     </div>
+
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-                            value={[value1, value2]}
+                            value={value2}
                             onChange={change}
+                            disableSwap
                         />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                        <span id={'hw11-value-2'} className={s.number}>{value2[value2.length - 1]}</span>
                     </div>
+
                 </div>
             </div>
         </div>
     )
 }
-
 export default HW11
